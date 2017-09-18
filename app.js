761575c597a22f2177ad6aad4829197d8dc968c9
Jesse Jaeger <jjaeger@rockislandauction.com>
2017-09-18 12:31:09 -0500

    Updates for npm publishing

---
+++ app.js
@@ -0,0 +1,33 @@
+exports.terminally-colorful = {
+	fg: {
+		black: 30,
+		red: 31,
+		green: 32,
+		yellow: 33,
+		blue: 34,
+		magenta: 35,
+		cyan: 36,
+		white: 37
+	},
+	bg: {
+		black: 40,
+		red: 41,
+		green: 42,
+		yellow: 43,
+		blue: 44,
+		magenta: 45,
+		cyan: 46,
+		white: 47
+	},
+	reset: 0,
+	color: function(f,b,bold,text) {
+		if(bold) {
+			return '\x1b[1m\x1b[' + this.fg[f] + 'm\x1b[' + this.bg[b] + 'm' + text + this.reset();
+		} else {
+			return '\x1b[2m\x1b[' + this.fg[f] + 'm\x1b[' + this.bg[b] + 'm' + text + this.reset();
+		}
+	},
+	reset: function() {
+		return '\x1b[0m';
+	}
+}
