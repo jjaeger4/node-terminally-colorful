2319b635e2bcd99bbe6ca20ea3c441d2ba9ea949
Jesse Jaeger <jjaeger@rockislandauction.com>
2017-09-18 14:31:52 -0500

    Module re-work for export issues, now call with tc.color(foreground,background,bold [bool],text)

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-exports['terminally-colorful'] = {
+const tc = {
 	fg: {
 		black: 30,
 		red: 31,
@@ -19,15 +19,15 @@ exports['terminally-colorful'] = {
 		cyan: 46,
 		white: 47
 	},
-	reset: 0,
-	color: function(f,b,bold,text) {
-		if(bold) {
-			return '\x1b[1m\x1b[' + this.fg[f] + 'm\x1b[' + this.bg[b] + 'm' + text + this.reset();
-		} else {
-			return '\x1b[2m\x1b[' + this.fg[f] + 'm\x1b[' + this.bg[b] + 'm' + text + this.reset();
-		}
-	},
 	reset: function() {
 		return '\x1b[0m';
+	}
+}
+
+exports.color = (f,b,bold,text) => {
+	if(bold) {
+		return '\x1b[1m\x1b[' + tc.fg[f] + 'm\x1b[' + tc.bg[b] + 'm' + text + tc.reset();
+	} else {
+		return '\x1b[2m\x1b[' + tc.fg[f] + 'm\x1b[' + tc.bg[b] + 'm' + text + tc.reset();
 	}
 }
